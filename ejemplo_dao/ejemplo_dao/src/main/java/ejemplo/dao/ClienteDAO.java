fef40a83464d31de628046c01d850cdb848317d0
Ivan Lavayen <ivanalejolavayen@gmail.com>
2026-08-31 16:53:37 -0300

    se crea la interface ClienteDao

--- ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/dao/ClienteDAO.java
+++ ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/dao/ClienteDAO.java
@@ -4,6 +4,12 @@ import ejemplo.entity.DetallePedido;
 
 import java.util.List;
 
-public interface ClienteDAO {
-
+public interface ClienteDao {
+    public int insertCliente(int idCliente, String nombre, String email) throws SQLException;
+    public void updateCliente(Cliente cliente) throws SQLException;
+    public void deleteCliente(int idCliente) throws SQLException;
+    public Cliente getCliente(int idCliente) throws SQLException;
+    public List<Cliente> getClientes() throws SQLException;
+    public List<Cliente> getMasFacturadosOrdenados() throws SQLException;
 }
+
