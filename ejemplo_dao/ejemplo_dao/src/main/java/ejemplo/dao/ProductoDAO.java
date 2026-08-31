89a5fe6106eb3d885607a43094a8c7be7ecef29b
Pablo Aguirre <pabloaaguirrejob@gmail.com>
2026-08-31 17:45:31 -0300

    agrego el productoDAO

--- ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/dao/ProductoDAO.java
+++ ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/dao/ProductoDAO.java
@@ -2,7 +2,12 @@ package ejemplo.dao;
 
 
 public interface ProductoDAO {
-
+    public int insertProducto(int idProducto, String nombre, int valor) throws SQLException;
+    public void updateProducto(Producto idProducto) throws SQLException;
+    public void deleteProducto(int idProducto) throws SQLException;
+    public Producto getProducto(int idProducto) throws SQLException;
+    public List<Producto> getProductos() throws SQLException;
+    public Producto getProductoMasVendido() throws SQLException;
 }
 
 
