c65dfe9422c741bf66b5b92d8f1a27dd5e28fcc8
CarolinaNavarrete <mnavarreteforgue@alumnos.exa.unicen.edu.ar>
2026-08-31 17:26:57 -0300

    subo archivo FacturaProductoDAO

--- ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/dao/FacturaProductoDAO.java
+++ ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/dao/FacturaProductoDAO.java
@@ -5,5 +5,9 @@ import ejemplo.entity.Producto;
 import java.util.List;
 
 public interface FacturaProductoDAO {
-
+    public int insertFacturaProducto(int idFactura, int idProducto, int cantidad) throws SQLException;
+    public void updateFacturaProducto(FacturaProducto facturaProducto) throws SQLException;
+    public void deleteFacturaProducto(int idFactura, int idProducto) throws SQLException;
+    public FacturaProducto getFacturaProducto(int idFactura, int idProducto) throws SQLException;
+    public List<FacturaProducto> getFacturasProductos() throws SQLException;
 }
