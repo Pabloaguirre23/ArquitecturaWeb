2ec5d3e7bacf6f1a742b558fb06be1a17bdb989d
Tiagoarra18 <arracheagrazianotiago@gmail.com>
2026-09-08 17:10:17 -0300

    Update Main.java

--- ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/Main.java
+++ ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/Main.java
@@ -1,14 +1,21 @@
-package ejemplo;
 import ejemplo.utils.ClientesMasFacturados;
-import ejemplo.utils.CargarTabSiNoExisten;
 import ejemplo.utils.ProductoMasRecaudado;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) throws SQLException {
+        CargarTabSiNoExisten CargarTabSiNoExisten = new CargarTabSiNoExisten();
+        CargarTabSiNoExisten.run();
+        //CargarDatosCSV CargarDatosCSV = new CargarDatosCSV();
+        //CargarDatosCSV.run();
 
+        ProductoMasRecaudado ProductoMasRecaudado = new ProductoMasRecaudado();
+        ProductoMasRecaudado.run();
+        ClientesMasFacturados ClientesMasFacturados = new ClientesMasFacturados();
+        ClientesMasFacturados.run();
     }
 
 }
