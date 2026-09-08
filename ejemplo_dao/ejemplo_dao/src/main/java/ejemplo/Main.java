aff368570f6f3e8205f55eac0d6297f201f1660d
ivanlavayen <63171475+ivanlavayen@users.noreply.github.com>
2026-09-08 16:19:43 -0300

    refactor del main para agregar consultas de cliente con mayor facturación y producto con más recaudación

--- ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/Main.java
+++ ejemplo_dao/ejemplo_dao/src/main/java/ejemplo/Main.java
@@ -1,16 +1,24 @@
 package ejemplo;
-// comentario para prueba de sincronizacion con git
-import ejemplo.factory.DAOFactory;
-import ejemplo.factory.DBType;
+
+import ejemplo.utils.CargarTabSiNoExisten;
+import ejemplo.utils.ClientesMasFacturados;
+import ejemplo.utils.ProductoMasRecaudado;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        DAOFactory f = DAOFactory.getInstance();
-        f.createClienteDAO();
-        f.createFacturaDAO();
-        f.createFacturaProductoDAO();
-        f.createProductoDAO();
+    public static void main(String[] args) throws SQLException {
+        CargarTabSiNoExisten CargarTabSiNoExisten = new CargarTabSiNoExisten();
+        CargarTabSiNoExisten.run();
+        //CargarDatosCSV CargarDatosCSV = new CargarDatosCSV();
+        //CargarDatosCSV.run();
+
+        ProductoMasRecaudado ProductoMasRecaudado = new ProductoMasRecaudado();
+        ProductoMasRecaudado.run();
+        ClientesMasFacturados ClientesMasFacturados = new ClientesMasFacturados();
+        ClientesMasFacturados.run();
     }
 
 }
